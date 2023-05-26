43981e6c4b5b9e995ca229d69b5c7dd8383cda74
mahmoud-badwy <mahmoudbadwysliem@gmail.com>
2023-05-26 15:07:30 +0300

    add sign in controller

--- lib/constants/controllers.dart
+++ lib/constants/controllers.dart
@@ -1,5 +1,6 @@
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
+import 'package:to_do_list_app/controller/sign_in_controller.dart';
 import '../controller/actions_controllers.dart';
 import '../controller/tasks_controller.dart';
 import '../controller/toggle_tabs.dart';
@@ -14,6 +15,9 @@ List<SingleChildWidget> myControllers() {
     ),
     ChangeNotifierProvider(
       create: (context) => ActionsController(),
+    ),
+    ChangeNotifierProvider(
+      create: (context) => SignInController(),
     )
   ];
 }
---
+++ lib/controller/sign_in_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class SignInController with ChangeNotifier {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+  TextEditingController nameTextEditingController = TextEditingController();
+}
